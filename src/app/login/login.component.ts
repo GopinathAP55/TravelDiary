fee0496278c04049fdabb2f30b9559dd3ea1e66d
GopinathAp55 <gopinath1993cse@gmail.com>
2021-01-08 20:59:21 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnChanges {
 
-  constructor() { }
-
+  constructor(private router : Router) { }
+  isGetOTPClicked : boolean = false;
+  buttonName : string = 'get OTP';
+  @Input() isLoggedIn  : boolean = false;
   ngOnInit(): void {
+    console.log('login')
   }
 
+  ngOnChanges(...args:any[]){
+    console.log(`ch:${this.isGetOTPClicked}`)
+  }
+
+  ngDocheck(){
+    
+  }
+
+  getOTP(){
+    this.isGetOTPClicked = true;
+    this.buttonName = 'Log in'
+    if(this.buttonName === 'Log in'){
+     
+
+    }
+
+  
+
+
+  }
+
+  logIn(){
+    console.log('login')
+    this.router.navigateByUrl('/home')
+  }
 }
